570e0ce431ab2e23c19b5f0ded3e0009592b3448
sabra55 <138562507+sabra55@users.noreply.github.com>
2023-07-30 19:03:44 +0200

    Update nav.js

--- projects/clutter/nav.js
+++ projects/clutter/nav.js
@@ -1,1 +1,1 @@
-document.body.innerHTML = "<div class=\"navbar\"><img src=clutter.svg /> | <a href=\"/projects/clutter/createclutter.html\">+</a></div><br>" + document.body.innerHTML;
+document.body.innerHTML = "<div class=\"navbar\"><img src=clutter.svg width=10% height=10% /> | <a href=\"/projects/clutter/createclutter.html\">+</a></div><br>" + document.body.innerHTML;
